8545488924cbf19f67fda5730cd691134a748817
Ninad3721 <ninadtrivedi25@gmail.com>
2023-07-29 16:40:37 +0530

    Summarizer added

---
+++ src/Summarizer.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+function Summarizer({ transcript }) {
+    const [summary, setSummary] = useState([]);
+    const handleSubmit = async () => {
+        const options = {
+            method: 'POST',
+            url: 'https://summarize-texts.p.rapidapi.com/pipeline',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': '4335290962msha7a7b91d666684bp1652ccjsnf92e4eee5a05',
+                'X-RapidAPI-Host': 'summarize-texts.p.rapidapi.com'
+            },
+            data: {
+                input: transcript
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            setSummary(response.data.output[0].text)
+            console.log(response.data.output[0].text);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return (
+        <div>
+            <button onClick={handleSubmit}>Summarize </button>
+            <p>{summary}</p>
+        </div>
+    )
+}
+
+export default Summarizer
+
--- src/Transcript.js
+++ src/Transcript.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
+import Summarizer from './Summarizer';
 
 function Transcript() {
+    const [transcript, setTranscript] = useState([])
+    let response;
     const handleSubmit = async () => {
         const options = {
             method: 'GET',
             url: 'https://youtube-subtitles-captions-downloader.p.rapidapi.com/ytmp3/ytmp3/subtitles/',
             params: {
-                url: 'https://www.youtube.com/watch?v=UJeSWbR6W04'
+                url: 'https://www.youtube.com/watch?v=rrdJOfb6Zkk'
             },
             headers: {
                 'X-RapidAPI-Key': '4335290962msha7a7b91d666684bp1652ccjsnf92e4eee5a05',
@@ -16,8 +19,9 @@ function Transcript() {
         };
 
         try {
-            const response = await axios.request(options);
-            console.log(response.data);
+            response = await axios.request(options);
+            console.log(response)
+            setTranscript(response.data.data)
         } catch (error) {
             console.error(error);
         }
@@ -25,7 +29,9 @@ function Transcript() {
 
     return (
         <div>
-            <button onClick={handleSubmit}>Click to summarize </button>
+            <button onClick={handleSubmit}> Transcript </button>
+            {transcript}
+            <Summarizer transcript={transcript} />
         </div>
     )
 }
